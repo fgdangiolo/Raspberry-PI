c8d2ee4b52d59eb21c8e36310760ef6f36279147
fdangiolo <dangiolofg@gmail.com>
2018-10-25 01:46:59 -0300

    Uploaded

---
+++ LAMP_Server/Readme.txt
@@ -0,0 +1,7 @@
+When you download this file, it must be given permissions and it must be executable. For this yu can do:
+
+sudo chmod u+x lamp.sh
+
+Then execute:
+
+./lamp.sh
---
+++ LAMP_Server/lamp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "======================="
+echo "Update and Upgrade..."
+echo "======================="
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+echo "======================"
+echo "Installing Apache..."
+echo "======================"
+
+# Install apache 
+sudo apt-get install apache2 -y
+
+echo "======================"
+echo "Start Apache..."
+echo "======================"
+
+# Enable the service apache2
+systemctl start apache2   
+systemctl enable apache2
+
+echo "======================"
+echo "Installing PHP..."
+echo "======================"
+
+# Install PHP
+sudo apt-get install php libapache2-mod-php -y
+
+# MYSQL: sudo mysql -u root -p
